591a2c488c0a51c004445e45e21a5f6ad882abed
Michael J. Brim <brimmj@ornl.gov>
2025-01-10 16:17:05 -0500

    add token managment program

--- olcf_api/token.py
+++ olcf_api/token.py
@@ -1,35 +1,42 @@
 import json
 import requests
 
+from typing import Tuple
+
 from .client import OLCFAPIClient
 
 class TokenService:
 
-    def __init__(self, service_name : str, api_client : OLCFAPIClient):
+    def __init__(self, api_client : OLCFAPIClient):
         self._client = api_client
         self._service_url = f'{api_client.base_url}/v1/token'
 
-    def get_token_info(self) -> bool:
+    def get_token_hash(self) -> str:
+        return self._client.api_token
+
+    def get_token_info(self) -> Tuple[bool, str]:
         token_url = f'{self._service_url}/ctls/introspect'
         
         response = requests.get(url=token_url,
                                 headers={"Authorization": f'{self._client.api_token}'})
         if response:
-            token_info = response.json()
-            self._token_info = json.dumps(token_info["token"], indent=4)
+            token_response = response.json()
+            self._token_info = json.dumps(token_response["token"], indent=4)
             print(f'INFO: Token {self._client.api_token}\n{self._token_info}')
-            return True
+            return True, self._token_info
         else:
-            print(f'GET from {token_url} failed - {response.status_code}')
-            return False
+            error = f'GET from {token_url} failed - {response.status_code}'
+            print(f'ERROR: {error}')
+            return False, error
 
-    def revoke_token(self, cluster_name : str) -> bool:
+    def revoke_token(self) -> Tuple[bool, str]:
         revoke_url = f'{self._service_url}/ctls/revoke'
 
         response = requests.delete(url=revoke_url,
                                    headers={"Authorization": f'{self._client.api_token}'})
         if response:
-            return True
+            return True, None
         else:
-            print(f'DEL {revoke_url} failed - {response.status_code}')
-            return False
+            error = f'DELETE {revoke_url} failed - {response.status_code}'
+            print(f'ERROR: {error}')
+            return False, error
