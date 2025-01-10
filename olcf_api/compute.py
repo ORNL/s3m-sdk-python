91b045b1ce75abc316e5141908b74c5b81f0e056
Michael J. Brim <brimmj@ornl.gov>
2025-01-10 15:35:22 -0500

    finish initial ComputeService implementation

--- olcf_api/compute.py
+++ olcf_api/compute.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
+from typing import List, Tuple
+
 from .client import OLCFAPIClient
 
 class ComputeService:
@@ -10,8 +12,7 @@ class ComputeService:
         self._cluster_name = cluster_name
         self._service_url = f'{api_client.base_url}/slurm/v0.0.42/{cluster_name}'
 
-
-    def list_jobs(self):
+    def list_jobs(self) -> Tuple[bool, str]:
         list_url = f'{self._service_url}/jobs'
 
         response = requests.get(url=list_url,
@@ -20,62 +21,69 @@ class ComputeService:
             job_list = response.json()
             jobs = json.dumps(job_list["jobs"], indent=4)
             print(f'INFO: Slurm Jobs on {self._cluster_name}\n{jobs}')
-            return True
+            return True, jobs
         else:
-            print(f'GET from {list_url} failed - {response.status_code}')
-            return False
+            error = f'GET from {list_url} failed - {response.status_code}'
+            print(error)
+            return False, error
 
     def submit_job(self,
                    project : str,
+                   workdir : str,
                    job_name : str,
                    job_queue : str,
-                   job_minutes : int = 5,
+                   script_contents : str = None,
+                   time_minutes : int = 1,
                    node_count : int = 1,
-                   task_count : int = 1,
-                   ) -> bool:
+                   env_vars : List[str] = None) -> Tuple[bool, str]:
 
         submit_url = f'{self._service_url}/job/submit'
         
+        ev_json_list = "[]"
+        if env_vars:
+            ev_json_list = json.dumps(env_vars)
+
         job_template = \
 '''{{
- "job": {{
-    "script": "{job_script_contents}",
-    "name": "{job_name}",
-    "account": "{account}",
-    "partition": "{queue}",
-    "current_working_directory": "/lustre/polis/{account}/proj-shared",
-    "environment": ["HELLO=world"],
-    "nodes": "{nodes}",
-    "tasks": {tasks},
-    "time_limit": {{
-        "number": {walltime},
-        "set":    true
+   "job": {{
+        "script": "{contents}",
+        "name": "{job_name}",
+        "account": "{account}",
+        "partition": "{queue}",
+        "current_working_directory": {cwd},
+        "environment": {env},
+        "nodes": "{nodes}",
+        "time_limit": {{
+            "number": {walltime},
+            "set": true
+        }}
     }}
-  }}
-}}
-'''
-        submit_request_str = job_template.format(job_script_contents=???,
+}}'''
+        submit_request_str = job_template.format(contents=script_contents,
                                                  job_name=job_name,
                                                  account=project,
                                                  queue=job_queue,
+                                                 cwd=workdir,
+                                                 env=ev_json_list,
                                                  nodes=node_count,
-                                                 tasks=task_count,
-                                                 walltime=job_minutes)
+                                                 walltime=time_minutes*60)
         #print(f'DEBUG: POST\n{submit_request_str}\n')
         submit_request = submit_request_str.encode()
         
         response = requests.post(url=submit_url, data=submit_request,
                                  headers={"Authorization": f'{self._client.api_token}', "Content-Type": "application/json"})
         if response:
-            submit_details = json.dumps(response.json(), indent=4)
+            submit_response = response.json()
+            submit_details = json.dumps(submit_response)
             #print(f'DEBUG: submit response\n{submit_details}')
-            TODO("extract jobid from response")
-            return True
+            jobid = json.dumps(submit_response["job_id"])
+            return True, jobid
         else:
-            print(f'POST to {submit_url} failed - {response.status_code}')
-            return False
+            error = f'POST to {submit_url} failed - {response.status_code}'
+            print(error)
+            return False, error
 
-    def get_job_info(self, jobid : str) -> bool:
+    def get_job_info(self, jobid : str) -> Tuple[bool, str]:
         cluster_url = f'{self._service_url}/job/{jobid}'
         
         response = requests.get(url=cluster_url,
@@ -84,7 +92,8 @@ class ComputeService:
             job_info = response.json()
             info = json.dumps(job_info["jobs"], indent=4)
             print(f'INFO: {self._cluster_name} Job Details\n{info}')
-            return True
+            return True, info
         else:
-            print(f'GET from {cluster_url} failed - {response.status_code}')
-            return False
+            error = f'GET from {cluster_url} failed - {response.status_code}'
+            print(error)
+            return False, error
