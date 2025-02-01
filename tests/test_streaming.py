e705f8e6282ee3abeac7868a2728ad75049ad5f5
McDonnell, Marshall <mcdonnellmt@ornl.gov>
2025-02-01 18:52:01 -0500

    Adds format fixes + test fix

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -3,3 +3,5 @@ from olcf_api.streaming import StreamingService
 
 def test_streaming_init(client):
     service = StreamingService("rabbitmq", client)
+    assert service._service_name == "rabbitmq"
+    assert service._client == client
